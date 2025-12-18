b3db864039c5d3a45ddeeb3d276402ff5d073c53
bala-amypo <bala@amypo.in>
2025-12-18 10:32:19 +0000

    User commit

--- src/main/java/com/example/demo/controller/CategoryController.java
+++ src/main/java/com/example/demo/controller/CategoryController.java
@@ -1,16 +1,17 @@
 package com.example.demo.controller;
 
 import com.example.demo.model.Category;
+import com.example.demo.model.User;
 import com.example.demo.service.CategoryService;
 import com.example.demo.service.UserService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.util.List;
+
 
 @RestController
 @RequestMapping("/categories")
