7c1ef1cd7097454ac6963d49629499207b81ecb8
bala-amypo <bala@amypo.in>
2025-12-18 08:30:07 +0000

    User commit

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -1,3 +1,15 @@
-public class UserService{
-    
+package com.example.finance.service;
+
+import com.example.finance.entity.User;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserService {
+
+    User saveUser(User user);
+
+    Optional<User> getUserById(Long id);
+
+    List<User> getAllUsers();
 }
