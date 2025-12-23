e0df71beec2d50c822e22dd2f6e9bb947bcdefab
bala-amypo <bala@amypo.in>
2025-12-23 05:38:23 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/BudgetSummaryServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/BudgetSummaryServiceImpl.java
@@ -1,4 +1,4 @@
-package com.example.demo.service;
+package com.example.demo.service.impl;
 
 import org.springframework.stereotype.Service;
 import com.example.demo.exception.BadRequestException;
--- src/main/java/com/example/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -1,4 +1,4 @@
-package com.example.demo.service;
+package com.example.demo.service.impl;
 
 import java.util.Optional;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
